3847061924a14c25c99dd556e7022dce0770168a
implObserver <prognozman@mail.ru>
2024-04-20 23:24:19 +0500

    style:add helmet

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import './App.css';
 import { Settings } from './components/originals/settings/Settings';
 import { Resume } from './components/originals/resume/Resume';
 import { appModel } from './main';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 export const App = () => {
   console.log(appModel)
+
   return (
-    <>
+    <HelmetProvider>
+      <Helmet>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
       <Settings></Settings>
       <Resume></Resume>
-    </>
+    </HelmetProvider>
   );
 }
 
