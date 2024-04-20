fb8b7adea8526cb912e49a2bace3e469dd3a2135
implObserver <prognozman@mail.ru>
2024-04-20 23:51:04 +0500

    style:ref mobile style

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,15 @@ import './App.css';
 import { Settings } from './components/originals/settings/Settings';
 import { Resume } from './components/originals/resume/Resume';
 import { appModel } from './main';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 export const App = () => {
   console.log(appModel)
 
   return (
-    <HelmetProvider>
-      <Helmet>
-        <meta name="viewport" content="width=device-width initial-scale=1.0" />
-        <title>CV-Application</title>
-        <meta charset="UTF-8" />
-      </Helmet>
+    <>
       <Settings></Settings>
       <Resume></Resume>
-    </HelmetProvider>
+    </>
   );
 }
 
