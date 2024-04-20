c4a13328c7e123003c5de37a91df1f8c3fc8d4f7
implObserver <prognozman@mail.ru>
2024-04-20 23:00:54 +0500

    style:ref mobile style

--- src/components/originals/resume/head/Head.jsx
+++ src/components/originals/resume/head/Head.jsx
@@ -5,7 +5,7 @@ import { appModel } from "../../../../main";
 
 export const ResumeHead = () => {
     const props = appModel.settings.container.sections.personalDetails;
-    const backgroundColor = State('#ffffff');
+    const backgroundColor = State('#ffa500');
 
     Object.assign(appModel.resume.updater, { [props.id]: {} });
     Object.assign(appModel.resume.styles.colors, { head: backgroundColor });
