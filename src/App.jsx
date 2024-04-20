1fb0ef263e628d6a1e12d5c52d326fd3b5edded0
implObserver <prognozman@mail.ru>
2024-04-20 23:41:51 +0500

    style:ref mobile style

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
   return (
     <HelmetProvider>
       <Helmet>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width initial-scale=1.0" />
         <title>CV-Application</title>
         <meta charset="UTF-8" />
       </Helmet>
